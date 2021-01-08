f84a19b0a773899883eefcc0b547c9c3ab91bd3b
avtuhsko2004 <avtuhsko2004@gmail.com>
2021-01-09 00:29:23 +0300

    a

---
+++ popup.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/popup.css">
+</head>
+<body>
+    <div class="popup">
+
+    </div>
+</body>
+<script src="scripts/auth-n-reg.js"></script>
+</html>
