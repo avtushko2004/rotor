5297b0ff4ef9083ea8fefd2c02319c49e80dba34
avtuhsko2004 <avtuhsko2004@gmail.com>
2021-01-09 21:06:53 +0300

    л

--- center.php
+++ center.php
@@ -245,13 +245,5 @@
 <script src="scripts/login.js"></script>
 <script src="scripts/index.js"></script>
 <script src="scripts/ajax.js"></script>
-  <script>
-                      document.getElementsByClassName('popup-cont')[0].style.display = "none";
-                        document.getElementsByClassName('lds-ring')[0].style.display = "block";
-                        setTimeout(function tick() {
-                            document.getElementsByClassName('lds-ring')[0].style.display = "none";
-                            document.getElementsByClassName('mailCode')[0].style.display = "block";
-                        }, 1000);
-  </script>
 </body>
 </html>
