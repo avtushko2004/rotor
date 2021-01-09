6d2f2539e35f847313ed79536330dbd75bbf14a7
avtuhsko2004 <avtuhsko2004@gmail.com>
2021-01-09 20:04:14 +0300

    a

--- scripts/login.js
+++ scripts/login.js
@@ -2,6 +2,7 @@ const blurPage = (props) => {
     const login = document.getElementById('login');
     const main = document.getElementsByTagName('main')[0];
     const header = document.getElementsByTagName('header')[0];
+    const body = document.getElementsByTagName('body')[0];
     if (props) {
         login.classList.toggle('displayWindow');
         main.classList.toggle('blurWindow');
@@ -19,23 +20,26 @@ let authLabel = document.getElementById('auth-h1');
 let regForm = document.getElementById('reg-form');
 let authForm = document.getElementById('auth-form');
 let regButton = document.getElementById('submit-btn');
-
-
+let goAuth = document.getElementById('go-auth');
+let goReg = document.getElementsByClassName('.go-reg');
 
 function authMake(){
     console.log("антон рак");
-    regLabel.classList.remove('disabled-h1');
-    authLabel.classList.add('disabled-h1');
+    // regLabel.classList.remove('disabled-h1');
+    // authLabel.classList.add('disabled-h1');
+    goAuth.classList.add('disabled');
     regForm.classList.add('disabled-form');
     authForm.classList.remove('disabled-form');
     regButton.classList.add('auth-btn');
     regButton.classList.remove('reg-btn');
     regButton.innerText = "Войти";
+
 }
 function regMake(){
     console.log("антон рак");
-    regLabel.classList.add('disabled-h1');
-    authLabel.classList.remove('disabled-h1');
+    // regLabel.classList.add('disabled-h1');
+    // authLabel.classList.remove('disabled-h1');
+    goReg.style.display = "block";
     regForm.classList.remove('disabled-form');
     authForm.classList.add('disabled-form');
     regButton.classList.remove('auth-btn');
