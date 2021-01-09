f436ee6508fd6d858948d60d77597807f12dd568
Anton <anbesed@gmail.com>
2021-01-09 13:12:57 +0300

    das

--- check-mail.php
+++ check-mail.php
@@ -5,3 +5,5 @@ require('base.php');
 $code = 'sS12Fg'; // код приходит с js
 $user = $db->prepare("UPDATE `users` SET `confirmation` = '1', `code` = NULL WHERE `users`.`code` = ?");
 $user->execute([$code]);
+
+// нужно ещё установить куки
