f4a2f12801f65ac93024bbdac5c97ee2e141467d
DmitryKolpakov <375295108919@mail.ru>
2021-01-10 12:27:18 +0300

    g

--- center.php
+++ center.php
@@ -8,11 +8,11 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <link rel="shortcut icon" href="icon.png" type="image/png">
+  <link rel="shortcut icon" href="images/icon.png" type="image/png">
   <link rel="stylesheet" href="styles/center.css">
   <link rel="stylesheet" href="styles/login.css">
   <link rel="stylesheet" href="styles/loader.css">
-  <title>Ротор-Про</title>
+  <title>rotor.pro</title>
 </head>
 <body ondragstart="drag(event)" style="opacity: 0;">
   <header>
